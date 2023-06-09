d66d044fe56a31cb44c2f9c4cc389fde9499165f
Álvaro José Agámez Licha <45150637+aagamezl@users.noreply.github.com>
2023-06-09 18:07:46 +0200

    feat(scoop): add uninstall.ps1 powershell script

---
+++ scoop/uninstall.ps1
@@ -0,0 +1,41 @@
+# Git
+scoop uninstall main/git
+
+# Add the extra buckets
+scoop bucket rm extras
+scoop bucket rm versions
+scoop bucket rm java
+scoop bucket rm nerd-fonts
+
+# Java
+scoop uninstall java/oraclejre8
+
+# Utils
+scoop uninstall main/7zip extras/totalcommander extras/powertoys extras/obs-studio extras/vlc extras/rufus
+
+# Graphics
+scoop uninstall extras/xnviewmp extras/paint.net
+
+# Colaboration
+scoop uninstall extras/slack extras/discord
+
+# Editors
+scoop uninstall extras/notepadplusplus
+
+# Browsers
+scoop uninstall versions/firefox-developer extras/brave
+
+# Dev Tools
+scoop uninstall versions/vscode-insiders extras/windows-terminal extras/postman extras/robo3t extras/dbeaver
+
+# Office
+scoop uninstall extras/wpsoffice extras/foxit-reader
+
+# Gaming
+scoop uninstall versions/steam
+
+# Music
+scoop uninstall extras/aimp
+
+# Fonts
+scoop uninstall nerd-fonts/FiraCode
