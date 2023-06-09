ab22d9cd94b035de1c77d018755a052a591a94b2
Álvaro José Agámez Licha <45150637+aagamezl@users.noreply.github.com>
2023-06-09 15:48:04 +0200

    feat(scoop): add app installation powershell script

---
+++ scoop/setup.ps1
@@ -0,0 +1,41 @@
+# Git
+scoop install main/git
+
+# Add the extra buckets
+scoop bucket add extras
+scoop bucket add versions
+scoop bucket add java
+scoop bucket add nerd-fonts
+
+# Java
+scoop install java/oraclejre8
+
+# Utils
+scoop install main/7zip extras/totalcommander extras/powertoys extras/windows-terminal extras/obs-studio extras/vlc extras/rufus extras/thisiswin11
+
+# Graphics
+scoop install extras/xnviewmp extras/paint.net
+
+# Colaboration
+scoop install extras/slack extras/discord
+
+# Editors
+scoop install versions/vscode-insiders extras/notepadplusplus
+
+# Browsers
+scoop install versions/firefox-developer extras/brave
+
+# Dev Tools
+scoop install extras/postman extras/robo3t extras/dbeaver
+
+# Office
+scoop install extras/wpsoffice extras/foxit-reader
+
+# Gaming
+scoop install versions/steam
+
+# Music
+scoop install extras/aimp
+
+# Fonts
+scoop install nerd-fonts/FiraCode
